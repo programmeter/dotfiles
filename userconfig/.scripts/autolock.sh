f63349623dc9f52af7e713b0673ade2d1c53ce01
programmeter <94227158+programmeter@users.noreply.github.com>
2024-08-08 12:26:01 +0200

    Bug fix

--- userconfig/.scripts/autolock.sh
+++ userconfig/.scripts/autolock.sh
@@ -4,6 +4,5 @@ playerctlstatus=$(playerctl status 2> /dev/null)
 playerctlstatusrandom=$(playerctl -p somethingsomething status 2> /dev/null)
 
 if [ "$playerctlstatus" = "$playerctlstatusrandom" ]; then
-    xset dpms force suspend
-    . ~/.scripts/lock.sh
+    xset dpms force suspend && ~/.scripts/lock.sh
 fi
