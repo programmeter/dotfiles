36fd160caaf108f38f54992e97a8c34200ef9ad7
programmeter <94227158+programmeter@users.noreply.github.com>
2024-08-20 13:11:12 +0200

    Bug fixes

--- dwm/.scripts/lock.sh
+++ dwm/.scripts/lock.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 setxkbmap us
-pkill picom
 dunstctl set-paused true && i3lock -i ~/.local/share/wallpapers/wallpaper-lock.png -u -n
-dunstctl set-paused false && setxkbmap -layout 'us,rs,rs' -variant ',latinyz,' -option 'caps:super'
-picom --config ~/.config/picom/picom.conf
-sleep 1
-. ~/.scripts/weather_status.sh
+dunstctl set-paused false && . ~/.scripts/kblayout.sh
